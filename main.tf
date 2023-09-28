165521a5a66754059fc95c05e7198f3cb52e5854
Máté Lang <langmatelaszlo@gmail.com>
2023-09-28 11:27:12 +0200

    adopted 1.1.0

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "uni-network" {
   source  = "app.terraform.io/matelang/uni-network/module"
-  version = "1.0.1"
+  version = "1.1.0"
 
   name        = var.name
   environment = var.environment
@@ -21,4 +21,8 @@ variable "environment" {
 
 variable "cidr" {
   type = string
+}
+
+output "public_subnet_ids" {
+  value = module.uni-network.public_subnet_ids
 }
